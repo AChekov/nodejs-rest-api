f335a7106f81ccd1f71a881fa70747c130c500a1
AChekov <4ekoff@gmail.com>
2022-09-28 22:11:55 +0300

    first settings

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
+
+// const dotenv = require("dotenv");
+// dotenv.config();
 
 const contactsRouter = require("./routes/api/contacts");
 
