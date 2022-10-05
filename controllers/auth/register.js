32f75efa59204c9802f91dc4c35a3ba62f4a6918
AChekov <4ekoff@gmail.com>
2022-10-05 23:33:00 +0300

    add user register

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,3 +1,22 @@
-const register = async (req, res) => {};
+const { User, schemas } = require("../../models/user");
+
+const { RequestError } = require("../../helpers");
+
+const register = async (req, res) => {
+  const { error } = schemas.registerSchema.validate(req.body);
+  if (error) {
+    throw RequestError(400, "missing required name field");
+  }
+  const { name, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw RequestError(409, "Email in use");
+  }
+  const result = await User.create({ name, email, password });
+  res.status(201).json({
+    name: result.name,
+    email: result.email,
+  });
+};
 
 module.exports = register;
