de6cdc664910e86554b3c49a831066f05ed0270f
AChekov <4ekoff@gmail.com>
2022-10-06 22:52:24 +0300

    modified:   models/user.js

--- models/user.js
+++ models/user.js
@@ -11,12 +11,12 @@ const userSchema = new Schema(
     },
     email: {
       type: String,
-      required: [true, 'Email is required'],,
+      required: [true, "Email is required"],
       unique: true,
     },
     password: {
       type: String,
-      required: [true, 'Password is required'],
+      required: [true, "Password is required"],
       minlength: 6,
     },
   },
